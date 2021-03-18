753fcf67310eb4fe2f5b241fdc8dc72b5c86d1f7
Prateek Reddy <prateekreddyy@gmail.com>
2021-03-18 20:40:12 +0530

    Remove burn functions

--- contracts/tokens/TidalToken.sol
+++ contracts/tokens/TidalToken.sol
@@ -4,20 +4,10 @@ pragma solidity 0.6.12;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // This token is owned by Timelock.
-contract TidalToken is ERC20("Tidal Token", "TIL") {
+contract TidalToken is ERC20("Tidal Token", "TIDAL") {
 
     constructor() public {
         _mint(msg.sender, 2e28);  // 20 billion, 18 decimals
     }
 
-    function burn(uint256 _amount) external {
-        _burn(_msgSender(), _amount);
-    }
-
-    function burnFrom(address account, uint256 amount) external {
-        uint256 currentAllowance = allowance(account, _msgSender());
-        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
-        _approve(account, _msgSender(), currentAllowance - amount);
-        _burn(account, amount);
-    }
 }
